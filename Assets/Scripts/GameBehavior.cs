8c0a30a5b4ea91d07ecdecb9e83f96365f51f7d4
regireed89 <regireed89@gmail.com>
2017-04-26 16:50:46 -0500

    Objects

---
+++ Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehavior : MonoBehaviour {
+
+
+    public List<GameObject> boxes;
+
+    public GameObject boxPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+        boxes = new List<GameObject>();
+	}
+	
+	void Update()
+    {
+        GameObject box;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            box = Instantiate(boxPrefab) as GameObject;
+            boxes.Add(box);
+        }
+    }
+}
