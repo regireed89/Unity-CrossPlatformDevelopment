de697f3a86db553843a524213adb688f2dc31947
regireed89 <regireed89@gmail.com>
2017-04-26 17:52:22 -0500

    tbag

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    private int i;
+    private int j;
+
+	// Use this for initialization
+	void Start () {
+		for(i = 0; i < 5; i++)
+        {
+            for(j = 0; j < 5; j++)
+            {
+
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserController : MonoBehaviour {
+    class Player
+    {
+        Player() { }
+        private Vector2 position = new Vector2(0, 0);
+        private int health = 100;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+	}
+}
