29b986a3d8c771cedcec5e6729edff63390623a0
regireed89 <regireed89@gmail.com>
2017-04-28 15:08:10 -0500

    work

---
+++ Assets/Scripts/CubeGrid.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGrid : MonoBehaviour
+{
+
+
+    public GameObject prefab;
+    public float offset;
+    private List<GameObject> cubes;
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+    [ContextMenu("Create")]
+    void ShowGrid()
+    {
+        GameObject cube;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                cube = Instantiate(prefab,this.transform) as GameObject;
+                cube.transform.position = new Vector3(i + offset, 0, j + offset);
+                cubes.Add(cube);
+            }
+        }
+    }
+    [ContextMenu("Destroy")]
+    void DstroyGrid()
+    {
+        foreach (GameObject item in cubes)
+        {
+            DestroyImmediate(item);
+        }
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/MoveBehavior.cs
+++ Assets/Scripts/MoveBehavior.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveBehavior : MonoBehaviour {
-
+    public int speed;
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float moveh = Input.GetAxis("Horizontal");
+        float movev = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveh, 0.0f, movev);
+        rb.AddForce(move * speed);
+    }
 }
